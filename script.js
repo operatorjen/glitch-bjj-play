24d73ce110564dae6f74a6c4b595bce68db6dbde
Glitch (hello-webpage) <none>
2019-01-06 16:22:04 +0000

    💈🐞 Checkpoint ./index.html:378534/426 ./script.js:378534/2296

--- script.js
+++ script.js
@@ -1,8 +1,60 @@
-if (pl.type.is_substitution(answer)) {
-  // Get the value of the food
-  var food = answer.lookup("X");
-  // Get the person
-  var person = name != "Y" ? name : answer.lookup("Y");
-  // Show answer
-  result.innerHTML = result.innerHTML + "<div>" + person + " likes " + food + "</div>";
+const start = document.querySelector('#start-position')
+const spositions = {
+  0: {
+    name: 'closed guard',
+    next_top: [
+
+    ],
+    next_bottom: [
+
+    ]
+  },
+  1: {
+    name: 'side mount',
+    next_top: [
+
+    ],
+    next_bottom: [
+
+    ]
+  },
+  2: {
+    name: 'half guard',
+    next_top: [
+
+    ],
+    next_bottom: [
+
+    ]
+  },
+  3: 'full mount',
+    next_top: [
+
+    ],
+    next_bottom: [
+
+    ]
+  },
+  4: {
+    name: 'back attack',
+    next_top: [
+
+    ],
+    next_bottom: [
+
+    ]
+  }
 }
+
+/*
+if (answer) {
+  if (pl.type.is_substitution(answer)) {
+    // Get the value of the food
+    var food = answer.lookup("X");
+    // Get the person
+    var person = name != "Y" ? name : answer.lookup("Y");
+    // Show answer
+    result.innerHTML = result.innerHTML + "<div>" + person + " likes " + food + "</div>";
+  }
+}
+*/
