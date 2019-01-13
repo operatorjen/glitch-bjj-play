b720abdc33a971029d8b336bc0c240c469363772
Glitch (hello-webpage) <none>
2019-01-13 16:04:53 +0000

    👬🦂 Checkpoint ./script.js:378534/473 ./style.css:378534/111

--- script.js
+++ script.js
@@ -104,6 +104,8 @@ const spositions = {
   }
 }
 
+ctx.beginPath()
+
 function generateItem(id) {
   let pos = document.createElement('li')
   pos.setAttribute('data-value', id)
@@ -125,19 +127,21 @@ function generateItem(id) {
         nextMoves(currentPosition)
         
         // render selected visual
-        //ctx.beginPath()
+        ctx.beginPath()
         ctx.strokeWidth = 10
-        ctx.strokeStyle = `rgb(${id * 0.1 * 100}, id * 255}, id * 100)`
+        ctx.strokeStyle = `rgb(${id * 255}, ${id * 255}, ${id * 100})`
+        
+        ctx.moveTo(ctx.width / 2, ctx.height / 2)
         
         if (id > 0) {
-          ctx.lineTo(Math.abs(Math.sin((id + 99) * 10) * ctx.width / 1.5), Math.abs(Math.cos((id) * 10) * ctx.height / 2))
+        //  ctx.lineTo(Math.abs(Math.sin(id * 10) * ctx.width / 1.5), Math.abs(Math.cos((id) * 10) * ctx.height / 2))
         }
-        ctx.stroke()
+        //ctx.stroke()
         
         //ctx.fillStyle = `rgba(20, ${(id + 100) * 255}, ${(id + 1) * 155}, 1.0)`
-        ctx.arc(Math.abs(Math.sin((id + 100) * 10) * ctx.width / 1.5), Math.abs(Math.cos((id + 1) * 10) * ctx.height / 2), 10, 0, 2 * Math.PI)
+        ctx.arc(Math.abs(Math.sin((id + 10) * 10) * ctx.width / 1.5), Math.abs(Math.cos((id + 1) * 10) * ctx.height / 2), 10, 0, 2 * Math.PI)
         ctx.stroke()
-        ctx.lineTo(Math.abs(Math.sin((id + 100) * 10) * ctx.width / 2), Math.abs(Math.cos((id + 1) * 10) * ctx.height / 2))
+        ctx.lineTo(Math.abs(Math.sin((id + 10) * 10) * ctx.width / 1.5), Math.abs(Math.cos((id) * 10) * ctx.height / 2))
         //console.log(Math.abs(Math.sin((id + 1) * 10) * ctx.width / 2), Math.abs(Math.cos((id + 1) * 10) * ctx.height / 2))
         ctx.stroke()
         //ctx.closePath()
@@ -151,7 +155,7 @@ function generateItem(id) {
     pos.textContent = 'SUBMISSION!'
  
     ctx.fillStyle = 'rgba(220, 10, 20, 1.0)'
-    ctx.arc(Math.abs(Math.sin((id + 100) * 10) * ctx.width / 2), Math.abs(Math.cos((id + 1) * 10) * ctx.height / 2), 10, 0, 2 * Math.PI)
+    ctx.arc(Math.abs(Math.sin((id + 10) * 10) * ctx.width / 2), Math.abs(Math.cos((id + 1) * 10) * ctx.height / 2), 10, 0, 2 * Math.PI)
     ctx.fill()
     //ctx.closePath()
     
