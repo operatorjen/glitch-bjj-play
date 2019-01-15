7f0dffe8cc874f454fef1a784273491f47411ae3
Glitch (hello-webpage) <none>
2019-01-15 20:31:01 +0000

    🐬😿 Checkpoint ./kinematics.js:378534/1181

--- kinematics.js
+++ kinematics.js
@@ -9,10 +9,42 @@
   let currentX = 0
   let currentY = 0
   
+  const body = {
+    neck: {
+      dependent: 'shoulders',
+      rotationEWMinDeg: 0,
+      rotationEWMaxDeg: 180,
+      rotationNSMinDeg: 335,
+      rotationNSMaxDeg: 205,
+      x:,
+      y:
+    },
+    core: {
+      dependent: ['hips', 'shoulders'],
+      rotationEWMinDeg: 0,
+      rotationEWMaxDeg: 180,
+      rotationNSMinDeg: 345,
+      rotationNSMaxDeg: 255
+    },
+    leg: {
+      dependent: 'hips',
+      rotationEWMinDeg: 0,
+      rotationEWMaxDeg: 180,
+      rotationNSMinDeg: 360,
+      rotationNSMaxDeg: 180
+    },
+    arm: {
+      dependent: 'shoulders',
+      rotationEWMinDeg: 0,
+      rotationEWMaxDeg: 180,
+      rotationNSMinDeg: 360,
+      rotationNSMaxDeg: 180
+    }
+  }
+  
   function render() {
-    ctx.beginPath()
     ctx.arc(currentX + (ctx.width / 2), currentY + (ctx.height / 2), 10, 0, 2 * Math.PI)
-    ctx.lineTo(currentX - 20, currentY)
+    ctx.lineTo(Math.sin(currentX), Math.cos(currentY))
     ctx.stroke()
     
     window.requestAnimationFrame(render) 
